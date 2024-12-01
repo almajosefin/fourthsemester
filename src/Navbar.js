5a9cffa7de367c7e70c3c390c7d727debe61594b
Alma Josefin Labrosse <125260073+almajosefin@users.noreply.github.com>
2024-12-01 17:16:45 +0100

    lowercase url

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,10 +6,10 @@ export default function Navbar() {
             <header>
                 <h1><a href="./">Portfolio</a></h1>
                 <ul>
-                    <li><a href="./Info">Overblik</a></li>
-                    <li><a href="./JavaScript">JavaScript</a></li>
-                    <li><a href="./EmbeddedC">Embedded C</a></li>
-                    <li><a href="./Log">Logbog</a></li>   
+                    <li><a href="./info">Overblik</a></li>
+                    <li><a href="./javascript">JavaScript</a></li>
+                    <li><a href="./embeddedc">Embedded C</a></li>
+                    <li><a href="./log">Logbog</a></li>   
                 </ul>
                 <p>Alma Josefin Labrosse, Gruppe 6, Datamatiker Online</p>      
             </header>
