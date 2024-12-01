7086364aa120362612246404073714aef6251027
Alma Josefin Labrosse <125260073+almajosefin@users.noreply.github.com>
2024-12-01 17:35:15 +0100

    app edits

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { HashRouter } from "react-router-dom";
 import './styles/index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HashRouter>
