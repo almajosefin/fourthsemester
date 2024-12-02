ea9e5c72f90114077c45ffafe472a5b63056635f
Alma Josefin Labrosse <125260073+almajosefin@users.noreply.github.com>
2024-12-02 02:59:27 +0100

    log

--- src/pages/Log.js
+++ src/pages/Log.js
@@ -5,8 +5,27 @@ export default function Log() {
         <div className="App">
             <header>
                 <h1>Logbog</h1>
-                <p>
-                </p>
+                <p>Uge 34</p>
+                <p>Mål</p>
+                <ul>
+                    <li>Finde virksomheder.</li>
+                    <li>Finde kildemateriale.</li>
+                </ul>
+                <p>Begrebsdannelse</p>
+                <ul>
+                    <li>JavaScript Essential Training, LinkedIn Learning.</li>
+                    <li>Arduino Starter Kit Project Book, 1 – 4.</li>
+                </ul>
+                <p>Aktiviteter</p>
+                <ul>
+                    <li>Møde med Procesgruppe 5.</li>
+                    <li>Kontakte virksomhed.</li>
+                </ul>
+                <p>Erfaring</p>
+                <ul>
+                    <li>Fundet virksomheder.</li>
+                    <li>Fundet kildemateriale.</li>
+                </ul>
             </header>
         </div>
     )
