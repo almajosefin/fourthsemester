5e5426afbd2cba6ace6f3170ec56c8d9a466a4af
Alma Josefin Labrosse <125260073+almajosefin@users.noreply.github.com>
2024-12-02 03:35:33 +0100

    log

--- src/pages/Log.js
+++ src/pages/Log.js
@@ -28,6 +28,244 @@ export default function Log() {
                             <li>Fundet virksomheder.</li>
                             <li>Fundet kildemateriale.</li>
                         </ul>
+                        <p>Uge 35</p>
+                        <p>Mål</p>
+                        <ul>
+                            <li>Kontakte en anden virksomhed.</li>
+                            <li>Finde yderligere kildemateriale.</li>
+                        </ul>
+                        <p>Aktiviteter</p>
+                        <ul>
+                            <li>Guildmøde med Webudvikling.</li>
+                            <li>Guildmøde med Embedded C.</li>
+                            <li>Produktvejledning.</li>
+                        </ul>
+                        <p>Erfaring</p>
+                        <ul>
+                            <li>Kontaktet en anden virksomhed.</li>
+                            <li>Fundet yderligere kildemateriale.</li>
+                        </ul>
+                        <p>Refleksion</p>
+                        <ul>
+                            <li>Det var godt, at vi hver især i gruppen, fandt flere virksomheder, så vi hurtigt kunne kontakte en anden, da den første trak sig.</li>
+                        </ul>
+                        <p>Uge 36</p>
+                        <p>Mål</p>
+                        <ul>
+                            <li>Afklare virksomhedens ønsker og behov.</li>
+                            <li>Skrive projektbeskrivelsen for Webudvikling og Embedded C.</li>
+                            <li>Lave HLD og LLD artefakter.</li>
+                        </ul>
+                        <p>Aktiviteter</p>
+                        <ul>
+                            <li>Møde med Procesgruppe 5.</li>
+                            <li>Møde med virksomhedsrepræsentant.</li>
+                            <li>HLD artefakter.</li>
+                        </ul>
+                        <p>Erfaring</p>
+                        <ul>
+                            <li>Vidensdeling af processer, og måder at gribe opgaven an på, til procesgruppemødet.</li>
+                            <li>Virksomhedens ønsker og behov.</li>
+                        </ul>
+                        <p>Refleksion</p>
+                        <ul>
+                            <li>Det er en fordel at forberede specifikke spørgsmål til virksomhedens krav og prioriteringer, og at have en forforståelse for forretningsprocesser.</li>
+                        </ul>
+                        <p>Uge 37</p>
+                        <p>Mål</p>
+                        <ul>
+                            <li>Lave LLD artefakter, og eventuelt tilrette HLD artefakter.</li>
+                        </ul>
+                        <p>Begrebsdannelse</p>
+                        <ul>
+                            <li>Arduino Starter Kit Project Book, 5 – 8.</li>
+                        </ul>
+                        <p>Aktiviteter</p>
+                        <ul>
+                            <li>Guildmøde med Webudvikling.</li>
+                            <li>Guildmøde med Embedded C.</li>
+                            <li>Produktvejledning.</li>
+                            <li>LLD artefakter.</li>
+                        </ul>
+                        <p>Refleksion</p>
+                        <ul>
+                            <li>Det vil være en god idé at forberede eventuelle spørgsmål til de næste guild møder.</li>
+                        </ul>
+                        <p>Uge 38</p>
+                        <p>Mål</p>
+                        <ul>
+                            <li>Definere læringsmål, der stemmer overens med projektbeskrivelserne.</li>
+                        </ul>
+                        <p>Begrebsdannelse</p>
+                        <ul>
+                            <li>Arduino Starter Kit, 9.</li>
+                        </ul>
+                        <p>Aktiviteter</p>
+                        <ul>
+                            <li>Møde med Procesgruppe 5.</li>
+                        </ul>
+                        <p>Refleksion</p>
+                        <ul>
+                            <li>Det er svært at definere læringsmålene præcist så tidligt i processen, da jeg især i Embedded C, stadig har meget begrænset viden om, hvad jeg kan lære.</li>
+                        </ul>
+                        <p>Uge 39</p>
+                        <p>Mål</p>
+                        <ul>
+                            <li>Opnå viden og løbende tilrette læringsmål.</li>
+                        </ul>
+                        <p>Begrebsdannelse</p>
+                        <ul>
+                            <li>Learn JavaScript, 1 - 15.</li>
+                            <li>React JS Full Course, YouTube.</li>
+                            <li>C Essential Learning, LinkedIn Learning.</li>
+                            <li>Arduino Starter Kit Project Book, 10 – 13.</li>
+                            <li>Learning Processing.</li>
+                        </ul>
+                        <p>Aktiviteter</p>
+                        <ul>
+                            <li>Guildmøde med Webudvikling.</li>
+                            <li>Guildmøde med Embedded C.</li>
+                            <li>Produktvejledning.</li>
+                        </ul>
+                        <p>Refleksion</p>
+                        <ul>
+                            <li>Første gang jeg leger med et Arduino display, hvor jeg gør mig tanker i forhold til et relevant projekt, jeg kan fremlægge for virksomhedsrepræsentanten til næste møde.</li>
+                        </ul>
+                        <p>Uge 40</p>
+                        <p>Mål</p>
+                        <ul>
+                            <li>Opnå viden og løbende tilrette læringsmål.</li>
+                            <li>Få feedback fra virksomhedsrepræsentant, og definere målsætning og kvalitetskriterier.</li>
+                        </ul>
+                        <p>Begrebsdannelse</p>
+                        <ul>
+                            <li>Learn JavaScript, 16 - 30.</li>
+                        </ul>
+                        <p>Aktiviteter</p>
+                        <ul>
+                            <li>Møde med Procesgruppe 5.</li>
+                            <li>Møde med virksomhedsrepræsentant.</li>
+                        </ul>
+                        <p>Erfaring</p>
+                        <ul>
+                            <li>Feedback fra virksomhedsrepræsentant, og thumbs up til kundeundersøgelses idé.</li>
+                        </ul>
+                        <p>Uge 41</p>
+                        <p>Mål</p>
+                        <ul>
+                            <li>Påbegynde de første eksperimenter før opstarten af kundeundersøgelsesprojektet, som det er defineret i målsætningen.</li>
+                        </ul>
+                        <p>Begrebsdannelse</p>
+                        <ul>
+                            <li>Learn JavaScript, 31 - 40.</li>
+                            <li>Master C Language Pointers på LinkedIn Learning.</li>
+                            <li>Arduino Starter Kit Project Book, 14.</li>
+                        </ul>
+                        <p>Aktiviteter</p>
+                        <ul>
+                            <li>Guildmøde med Webudvikling.</li>
+                            <li>Guildmøde med Embedded C.</li>
+                            <li>Produktvejledning.</li>
+                            <li>Kundeundersøgelse: arduino knap udskriver værdi i arduino consol.</li>
+                        </ul>
+                        <p>Erfaring</p>
+                        <ul>
+                            <li>Jeg har sammensat en fysisk Arduino knap, og fået den til at udskrive en værdi til konsollen, når man trykker på den.</li>
+                        </ul>
+                        <p>Uge 43</p>
+                        <p>Mål</p>
+                        <ul>
+                            <li>Fortsætte de første eksperimenter før opstarten af kundeundersøgelsesprojektet, som det er  defineret i målsætningen.</li>
+                        </ul>
+                        <p>Begrebsdannelse</p>
+                        <ul>
+                            <li>Learn JavaScript, 41 - 62.</li>
+                            <li>Arduino Starter Kit Project Book, 15.</li>
+                        </ul>
+                        <p>Aktiviteter</p>
+                        <ul>
+                            <li>Møde med Procesgruppe 5.</li>
+                            <li>Kundeundersøgelse: processing knap udskriver tekst i processing consol.</li>
+                        </ul>
+                        <p>Erfaring</p>
+                        <ul>
+                            <li>Tegne en knap i processing, og få den til at skrive en værdi ud til konsollen, når man trykker på den. </li>
+                        </ul>
+                        <p>Uge 44</p>
+                        <p>Mål</p>
+                        <ul>
+                            <li>Opstart af kundeundersøgelsesprojektet, som det er defineret i målsætningen.</li>
+                        </ul>
+                        <p>Begrebsdannelse</p>
+                        <ul>
+                            <li>Learn React, Scrimba.</li>
+                        </ul>
+                        <p>Aktiviteter</p>
+                        <ul>
+                            <li>Guildmøde med Webudvikling.</li>
+                            <li>Guildmøde med Embedded C.</li>
+                            <li>Produktvejledning.</li>
+                            <li>Kundeundersøgelse: arduino touch display knap udskriver værdi i processing consol via serialport.</li>
+                        </ul>
+                        <p>Refleksion</p>
+                        <ul>
+                            <li>Det er godt, at have en at sparre med, når meget af det yderligere Arduino materiale ikke virker super gennemskueligt for en begynder.</li>
+                        </ul>
+                        <p>Uge 45</p>
+                        <p>Mål</p>
+                        <ul>
+                            <li>Fortsættelse af kundeundersøgelsesprojektet, som det er defineret i målsætningen, med fokus på forbindelse til WiFi.</li>
+                        </ul>
+                        <p>Aktiviteter</p>
+                        <ul>
+                            <li>Møde med Procesgruppe 5.</li>
+                            <li>Kundeundersøgelse: forbind arduino til WiFi.</li>
+                        </ul>
+                        <p>Erfaring</p>
+                        <ul>
+                            <li>Ufatteligt meget at tiden er gået på at bakse med, at få Arduino til at forbinde til internettet, uden at hoppe af.</li>
+                        </ul>
+                        <p>Refleksion</p>
+                        <ul>
+                            <li>Der er ikke så meget brugbart kildemateriale tilgængeligt til de lige præcis de resultater, jeg ønsker at opnå ved brug af Arduino.</li>
+                        </ul>
+                        <p>Uge 46</p>
+                        <p>Mål</p>
+                        <ul>
+                            <li>Få min Arduino til at udskrive et json object, hver gang man trykker på en knap.</li>
+                        </ul>
+                        <p>Aktiviteteer</p>
+                        <ul>
+                            <li>Guildmøde med Webudvikling.</li>
+                            <li>Guildmøde med Embedded C.</li>
+                            <li>Produktvejledning.</li>
+                            <li>Kundeundersøgelse: udskriv json object.</li>
+                        </ul>
+                        <p>Erfaring</p>
+                        <ul>
+                            <li>Jeg har fået min Arduino til at udskrive et json object, hver gang man trykker på en knap.</li>
+                        </ul>
+                        <p>Refleksioner</p>
+                        <ul>
+                            <li>Næste gang, vil jeg prøve at få det til at virke, uden at bruge tid på, at prøve at forbinde til WiFi først.</li>
+                        </ul>
+                        <p>Uge 47</p>
+                        <p>Mål</p>
+                        <ul>
+                            <li>Få min Arduino til at poste data til webapplikationen.</li>
+                        </ul>
+                        <p>Aktiviteter</p>
+                        <ul>
+                            <li>Kundeundersøgelse: forbind til urelateret server og udfør GET.</li>
+                        </ul>
+                        <p>Erfaring</p>
+                        <ul>
+                            <li>Jeg har lært at få min Arduino til at søge på google for mig.</li>
+                        </ul>
+                        <p>Refleksion</p>
+                        <ul>
+                            <li>Jeg har fundet ud af, det ikke er så let at tilgå vores API, efter der er implementeret IT-sikkerheds tiltag.</li>
+                        </ul>
                     </div>
                 </div>
             </header>
