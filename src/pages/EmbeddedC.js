7f66fabb8aa9a6643436121a4c67a5dce99265fa
Alma Josefin Labrosse <125260073+almajosefin@users.noreply.github.com>
2024-12-09 11:44:30 +0100

    edits

--- src/pages/EmbeddedC.js
+++ src/pages/EmbeddedC.js
@@ -6,7 +6,6 @@ export default function EmbeddedC() {
         <div className="App">
             <header>
                 <h1>Embedded C</h1>
-                <video src="https://github.com/almajosefin/fourthsemester/blob/main/assets/sketch7arduinvid.mp4"type="video/mp4" width="320" height="240"></video>
             </header>
         </div>
     )
