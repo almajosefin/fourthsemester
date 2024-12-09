5581c7bd3f3f5c9d484213b8da66533cd57ca53b
Alma Josefin Labrosse <125260073+almajosefin@users.noreply.github.com>
2024-12-09 11:36:43 +0100

    video

--- src/pages/EmbeddedC.js
+++ src/pages/EmbeddedC.js
@@ -6,7 +6,7 @@ export default function EmbeddedC() {
         <div className="App">
             <header>
                 <h1>Embedded C</h1>
-                <video src="https://github.com/almajosefin/fourthsemester/blob/main/assets/sketch7arduinvid.mp4"type="video/mp4" width="320" height="240" controls autoplay muted playsInline></video>
+                <video src="https://github.com/almajosefin/fourthsemester/blob/main/assets/sketch7arduinvid.mp4"type="video/mp4" width="320" height="240"></video>
             </header>
         </div>
     )
