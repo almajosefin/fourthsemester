4fed4a1f5e3c075fb6be5a758592f717b886c5a5
Alma Josefin Labrosse <125260073+almajosefin@users.noreply.github.com>
2024-12-09 13:09:11 +0100

    video

--- src/pages/EmbeddedC.js
+++ src/pages/EmbeddedC.js
@@ -7,7 +7,7 @@ export default function EmbeddedC() {
             <header>
                 <h1>Embedded C</h1>
                 <div>
-                    <video src="https://ik.imagekit.io/ikmedia/example_video.mp4" controls autoplay muted playsInline></video>
+                    <video src="https://github.com/almajosefin/fourthsemester/blob/main/src/assets/sketch7arduinvid.mp4" controls autoplay muted playsInline></video>
                 </div>
             </header>
         </div>
