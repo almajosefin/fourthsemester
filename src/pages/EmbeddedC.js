27e2175e2e4fac6b9d5be4b5a7ed814db5485432
Alma Josefin Labrosse <125260073+almajosefin@users.noreply.github.com>
2024-12-09 11:21:17 +0100

    assets

--- src/pages/EmbeddedC.js
+++ src/pages/EmbeddedC.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import './videos/sketch7arduinvid.mp4';
+import './assets/sketch7arduinvid.mp4';
 
 export default function EmbeddedC() {
     return(
         <div className="App">
             <header>
                 <h1>Embedded C</h1>
-                <video src="https://github.com/almajosefin/fourthsemester/blob/main/videos/sketch7arduinvid.mp4"type="video/mp4" width="320" height="240" controls autoplay muted playsInline></video>
+                <video src="https://github.com/almajosefin/fourthsemester/blob/main/assets/sketch7arduinvid.mp4"type="video/mp4" width="320" height="240" controls autoplay muted playsInline></video>
             </header>
         </div>
     )
