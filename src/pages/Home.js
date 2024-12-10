20653151cb0c8ef9a942ca41d87bec6489640320
Alma Josefin Labrosse <125260073+almajosefin@users.noreply.github.com>
2024-12-10 15:07:39 +0100

    homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,20 +6,26 @@ export default function Home() {
         <div className="App">
             <header>
                 <h1>Velkommen</h1>
-                <p>Alma Josefin Labrosse</p>
-                <p>Fjerde Semester 2024</p>
-                <p>Datamatiker Online</p>
-                <p>UCL Erhvervsakademi & Professionshøjskole</p>
-                <p>Webudvikling (React & JavaScript): 15 ECTS</p>
-                <p>Embedded C (Arduino & C): 15 ECTS</p>
-                <p>Gruppe 6: Maja Daniela Hansen, Justin Anthony Kapelke & Emre Altintas</p>
-                <p>Procesvejleder: Diaa Zobair Shollar</p>
-                <p>Guildvejleder: Tue Hellstern</p>
-                <p><a href="https://github.com/almajosefin/fourthsemester?tab=readme-ov-file">ReadMe</a></p>
-                <p><a href="https://github.com/DatamatikerTeam6/Majussmykkerklient.git">MajusSmykkerKLient</a></p>
-                <p><a href="https://github.com/DatamatikerTeam6/MajusSmykker.git">MajusSmykkerAPI</a></p>
-                <p><a href="https://github.com/almajosefin/arduino.git">Arduino</a></p>
-                <div><YouTube videoId="DMAHtpleZT0"/></div>
+                <div className="App-row">
+                    <div className="App-column">
+                        <p>Alma Josefin Labrosse</p>
+                        <p>Fjerde Semester 2024</p>
+                        <p>Datamatiker Online</p>
+                        <p>UCL Erhvervsakademi & Professionshøjskole</p>
+                        <p>Webudvikling (React & JavaScript): 15 ECTS</p>
+                        <p>Embedded C (Arduino & C): 15 ECTS</p>
+                        <p>Gruppe 6: Maja Daniela Hansen, Justin Anthony Kapelke & Emre Altintas</p>
+                        <p>Procesvejleder: Diaa Zobair Shollar</p>
+                        <p>Guildvejleder: Tue Hellstern</p>
+                    </div>
+                    <div className="App-column">
+                        <YouTube videoId="DMAHtpleZT0"/>
+                        <p><a href="https://github.com/almajosefin/fourthsemester?tab=readme-ov-file">ReadMe</a></p>
+                        <p><a href="https://github.com/DatamatikerTeam6/Majussmykkerklient.git">MajusSmykkerKLient</a></p>
+                        <p><a href="https://github.com/DatamatikerTeam6/MajusSmykker.git">MajusSmykkerAPI</a></p>
+                        <p><a href="https://github.com/almajosefin/arduino.git">Arduino</a></p>
+                    </div>
+                </div>
             </header>
         </div>
     )
